8b5b0ac1f1fe1c88d41533d24130d38e93b8a891
MatGhp <mat.ghp@gmail.com>
2024-07-08 18:37:03 +0200

    add tasks service  & Get method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
-import { TasksController } from './task/controllers/tasks/tasks.controller';
 
 @Module({
   imports: [TaskModule],
-  controllers: [TasksController],
   providers: [TaskModule],
 })
 export class AppModule {}
