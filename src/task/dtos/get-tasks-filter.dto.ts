cb565e5496d5262fc856751b6ea622ccb75e77b5
MatGhp <mat.ghp@gmail.com>
2024-07-10 14:00:35 +0200

    add validation to GetTasksFilterDto

--- src/task/dtos/get-tasks-filter.dto.ts
+++ src/task/dtos/get-tasks-filter.dto.ts
@@ -1,7 +1,16 @@
 import { TaskStatus } from '../models/task.model';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   title?: string;
+
+  @IsOptional()
+  @IsString()
   description?: string;
 }
